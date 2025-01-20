9e52fdeabd64c122333b6964e170ee233c706089
안형준 <anhyeongjun@anhyeongjun-ui-MacBookAir.local>
2025-01-20 17:41:49 +0900

    fix: 웹소켓 연결 엔드포인트 수정

--- src/main/java/com/sweetme/back/common/config/WebSocketConfig.java
+++ src/main/java/com/sweetme/back/common/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/studies/{studyId}/chat").
+        registry.addHandler(socketHandler, "/ws/studies/{studyId}/chat").
                 setAllowedOriginPatterns("*");
     }
 
